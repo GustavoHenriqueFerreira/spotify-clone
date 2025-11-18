94382ac7d740ad5ae589f19fd0e1202c9ae33a8c
GustavoHenriqueFerreira <gustavohferreiraalves@gmail.com>
2025-11-18 12:54:14 -0300

    cleanup: Remoção de coisas desnecessárias

--- src/App.tsx
+++ src/App.tsx
@@ -22,31 +22,14 @@ import { persistor, store, useAppDispatch, useAppSelector } from './store/store'
 import WebPlayback, { WebPlaybackProps } from './utils/spotify/webPlayback';
 
 // Pages
-import SearchContainer from './pages/Search/Container';
 import { playerService } from './services/player';
 import { Spinner } from './components/spinner/spinner';
 
 const Home = lazy(() => import('./pages/Home'));
 const Page404 = lazy(() => import('./pages/404'));
 const AlbumView = lazy(() => import('./pages/Album'));
-const GenrePage = lazy(() => import('./pages/Genre'));
-const BrowsePage = lazy(() => import('./pages/Browse'));
 const ArtistPage = lazy(() => import('./pages/Artist'));
 const PlaylistView = lazy(() => import('./pages/Playlist'));
-const ArtistDiscographyPage = lazy(() => import('./pages/Discography'));
-
-const Profile = lazy(() => import('./pages/User/Home'));
-const ProfileTracks = lazy(() => import('./pages/User/Songs'));
-const ProfileArtists = lazy(() => import('./pages/User/Artists'));
-const ProfilePlaylists = lazy(() => import('./pages/User/Playlists'));
-
-const SearchPage = lazy(() => import('./pages/Search/Home'));
-const SearchTracks = lazy(() => import('./pages/Search/Songs'));
-const LikedSongsPage = lazy(() => import('./pages/LikedSongs'));
-const SearchAlbums = lazy(() => import('./pages/Search/Albums'));
-const SearchPlaylist = lazy(() => import('./pages/Search/Playlists'));
-const SearchPageArtists = lazy(() => import('./pages/Search/Artists'));
-const RecentlySearched = lazy(() => import('./pages/Search/RecentlySearched'));
 
 window.addEventListener('resize', () => {
   const vh = window.innerWidth;
@@ -114,52 +97,13 @@ const RoutesComponent = memo(() => {
     () =>
       [
         { path: '', element: <Home container={container} />, public: true },
-        { path: '/collection/tracks', element: <LikedSongsPage container={container} /> },
         {
           public: true,
           path: '/playlist/:playlistId',
           element: <PlaylistView container={container} />,
         },
         { path: '/album/:albumId', element: <AlbumView container={container} /> },
-        {
-          path: '/artist/:artistId/discography',
-          element: <ArtistDiscographyPage container={container} />,
-        },
         { public: true, path: '/artist/:artistId', element: <ArtistPage container={container} /> },
-        { path: '/users/:userId/artists', element: <ProfileArtists container={container} /> },
-        { path: '/users/:userId/playlists', element: <ProfilePlaylists container={container} /> },
-        { path: '/users/:userId/tracks', element: <ProfileTracks container={container} /> },
-        { path: '/users/:userId', element: <Profile container={container} /> },
-        { public: true, path: '/genre/:genreId', element: <GenrePage /> },
-        { public: true, path: '/search', element: <BrowsePage /> },
-        { path: '/recent-searches', element: <RecentlySearched /> },
-        {
-          public: true,
-          path: '/search/:search',
-          element: <SearchContainer container={container} />,
-          children: [
-            {
-              path: 'artists',
-              element: <SearchPageArtists container={container} />,
-            },
-            {
-              path: 'albums',
-              element: <SearchAlbums container={container} />,
-            },
-            {
-              path: 'playlists',
-              element: <SearchPlaylist container={container} />,
-            },
-            {
-              path: 'tracks',
-              element: <SearchTracks container={container} />,
-            },
-            {
-              path: '',
-              element: <SearchPage container={container} />,
-            },
-          ],
-        },
         { path: '*', element: <Page404 /> },
       ].filter((r) => (user ? true : r.public)),
     [container, user]
@@ -186,7 +130,7 @@ const RoutesComponent = memo(() => {
               path={route.path}
               element={<Suspense>{route.element}</Suspense>}
             >
-              {route?.children
+              {/* {route?.children
                 ? route.children.map((child) => (
                     <Route
                       key={child.path}
@@ -194,7 +138,7 @@ const RoutesComponent = memo(() => {
                       element={<Suspense>{child.element}</Suspense>}
                     />
                   ))
-                : undefined}
+                : undefined} */}
             </Route>
           ))}
         </Routes>
